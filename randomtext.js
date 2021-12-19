e824d659a65863b148d22731c0e6aa22cdccbf3f
Paveun <a.tresierrajansen@gmail.com>
2021-12-19 17:28:05 -0500

    random text added
    Changed header2 to be randomly generated from an array of sentences

---
+++ randomtext.js
@@ -0,0 +1,10 @@
+var r_text = new Array ();
+r_text[0] = "Are you supposed to be here?";
+r_text[1] = "Makes computers go brr";
+r_text[2] = "Working hard to hardly work";
+r_text[3] = "Capable of answering skill testing questions";
+r_text[4] = "Is definitely not a robot.";
+
+var i = Math.floor(r_text.length * Math.random());
+
+document.write(r_text[i]);
